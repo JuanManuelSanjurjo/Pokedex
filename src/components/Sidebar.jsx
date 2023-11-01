7a4edf50dcdc0ea34659b191bbd983450916158c
JuanManuelSanjurjo <juanmanuelsanjurjo@hotmail.com>
2023-10-31 21:43:35 -0300

    images fix

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ function Sidebar({setSidebar}) {
   return (
     <div className='sidebar' id='sidebar' onClick={closeSidbar}>
           <h2 className='sidebarTitle '>Popular links </h2>
-          <a className='sideBarLink' href='https://pokeapi.co/' target='_blank' rel='noreferrer'>PokeApi <img className="pokeballImgSmall" src='./public/pokeball.png'></img></a>
+          <a className='sideBarLink' href='https://pokeapi.co/' target='_blank' rel='noreferrer'>PokeApi <img className="pokeballImgSmall" src='./public\pokeball.png'></img></a>
           <a className='sideBarLink' href='https://pokemon.fandom.com/es/wiki/Pok%C3%A9mon_Wiki' target='_blank' rel='noreferrer'>Pokemon Wiki <img className="pokeballImgSmall" src='./public/pokeball.png'></img></a>
           <a className='sideBarLink' href='https://www.pokemon.com/es' target='_blank' rel='noreferrer'>Official Site <img className="pokeballImgSmall" src='./public/pokeball.png'></img></a>
           <a className='sideBarLink' href='https://pokeapi.co/' target='_blank' rel='noreferrer'>Link a algo <img className="pokeballImgSmall" src='./public/pokeball.png'></img></a>
